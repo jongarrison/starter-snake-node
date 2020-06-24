0327255c2c56123012ebfe8059c6a3a3df7a385c
Jon Garrison <jongarrison@gmail.com>
2020-06-24 20:21:34 +0000

    Minor console additions

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ app.get('/', handleIndex)
 app.post('/start', handleStart)
 app.post('/move', handleMove)
 app.post('/end', handleEnd)
-
+console.log("JG Version 2");
 app.listen(PORT, () => console.log(`Battlesnake Server listening at http://127.0.0.1:${PORT}`))
 
 
 function handleIndex(request, response) {
+  console.log("event=requestIndex");
   var battlesnakeInfo = {
     apiversion: '1',
     author: '',
@@ -27,6 +28,8 @@ function handleIndex(request, response) {
 
 function handleStart(request, response) {
   var gameData = request.body
+  console.log("event=handleStart gameData=");
+  console.log(gameData);
 
   console.log('START')
   response.status(200).send('ok')
@@ -34,6 +37,9 @@ function handleStart(request, response) {
 
 function handleMove(request, response) {
   var gameData = request.body
+
+  console.log("event=handleMove gameData=");
+  console.log(gameData);
 
   var possibleMoves = ['up', 'down', 'left', 'right']
   var move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)]
