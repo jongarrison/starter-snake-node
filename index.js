c6c449f042bb2da80fb3309e30ae9f1444470459
Jon Garrison <jongarrison@gmail.com>
2020-06-25 07:31:44 +0000

    Simple spinning snake based on Gage's advice

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ function handleIndex(request, response) {
   var battlesnakeInfo = {
     apiversion: '1',
     author: '',
-    color: '#888888',
-    head: 'default',
-    tail: 'default'
+    color: '#001000',
+    head: 'smile',
+    tail: 'bolt'
   }
   response.status(200).json(battlesnakeInfo)
 }
@@ -39,11 +39,25 @@ function handleMove(request, response) {
   var gameData = request.body
 
   console.log("event=handleMove gameData=");
-  console.log(gameData);
-
-  var possibleMoves = ['up', 'down', 'left', 'right']
-  var move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)]
-
+  console.dir(gameData,{depth:null});
+ 
+  //var possibleMoves = ['up', 'down', 'left', 'right']
+  //var move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)]
+  
+  console.log("turn = "+gameData.turn)
+  
+  //move = 'left'+'down'+'right'+'up'
+  if (gameData.turn%4===0){
+    move = "left"
+  }else if(gameData.turn%4===1){
+    move = "up"
+  }else if(gameData.turn%4===2){
+    move = "right"
+  }else if(gameData.turn%4===3){
+    move = "down"
+  }
+  console.log("head@")
+  console.dir(gameData.you.head)
   console.log('MOVE: ' + move)
   response.status(200).send({
     move: move
